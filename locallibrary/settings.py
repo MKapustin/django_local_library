b6f6281ec0cd2643664026cf22014f15d7b5ea88
MKapustin <maxkapustin@mail.ru>
2019-07-18 16:52:31 +0300

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -141,3 +141,5 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+ALLOWED_HOSTS = ['guarded-ravine-72781.herokuapp.com']
